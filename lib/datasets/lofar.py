d807f2ee0888c23eb7b23de96b976aa6a251e2cb
Rafael Mostert Tritanium <mostert@strw.leidenuniv.nl>
2019-04-05 00:16:42 +0200

    changed n_comp class values from int to string

--- lib/datasets/lofar.py
+++ lib/datasets/lofar.py
@@ -47,7 +47,7 @@ class lofar(imdb):
             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'LOFAR' + self._dataset_version)
         self._classes = ('__background__',  # always index 0
-                         0, 1, 2, 3, 4, 5)
+                         '0', '1', '2', '3', '4', '5')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
