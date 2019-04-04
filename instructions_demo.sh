ad3c350ea19acb713b36a4fe397ee2bb706266b4
Rafael Mostert Tritanium <mostert@strw.leidenuniv.nl>
2019-04-04 17:12:29 +0200

    added bash scripts to run test/train/demo

---
+++ instructions_demo.sh
@@ -0,0 +1,16 @@
+# Activate virtualenv containing pytorch 1.0 and opencv, etc.
+echo "Activating conda env."
+source activate pytorch0.4.0
+
+GPU_ID=1
+SESSION=1
+EPOCH=1
+CHECKPOINT=2504
+
+echo "Run demo.py"
+CUDA_VISIBLE_DEVICES=$GPU_ID python demo.py --net vgg16 \
+                   --checksession $SESSION --checkepoch $EPOCH --checkpoint $CHECKPOINT \
+                   --cuda --image_dir images/variable_size_cutouts --load_dir models
+
+echo "Deactivating conda env."
+conda deactivate
