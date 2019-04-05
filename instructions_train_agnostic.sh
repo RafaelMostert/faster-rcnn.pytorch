b3387989675114eb593fce8fd5fafed43c0de0b5
Rafael Mostert Tritanium <mostert@strw.leidenuniv.nl>
2019-04-05 12:58:30 +0200

    added file

---
+++ instructions_train_agnostic.sh
@@ -0,0 +1,23 @@
+echo "Start train script! (test time: unknown yet)"
+
+# Activate virtualenv containing pytorch 0.4.0 and opencv, etc.
+echo "Activating conda env."
+source activate pytorch0.4.0
+
+GPU_ID=4
+SESSION=2
+BATCH_SIZE=4
+WORKER_NUMBER=1
+LEARNING_RATE=0.001
+DECAY_STEP=3  # Measured in epochs, decay will possibly only take place once?
+echo "Run lofar_trainval_net.py"
+CUDA_VISIBLE_DEVICES=$GPU_ID python lofar_trainval_net.py \
+                   --dataset lofar --net vgg16 \
+                   --bs $BATCH_SIZE --nw $WORKER_NUMBER \
+                   --s $SESSION \
+                   --lr $LEARNING_RATE --lr_decay_step $DECAY_STEP \
+                   --cuda --cag
+# Note that class agnostig is set to true now
+
+echo "Deactivating conda env."
+conda deactivate
